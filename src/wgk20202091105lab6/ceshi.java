cd73deeb7f30bef8c3cb728f22503a2595737f1b
nlnlbnl <ws744108278@icloud.com>
2022-04-08 13:33:21 +0800

    /Users/kuogongzi/IdeaProjects/wgk20202091105/src/wgk20202091105lab2/MyTriangle.java

--- src/wgk20202091105lab6/ceshi.java
+++
@@ -1,9 +0,0 @@
-package wgk20202091105lab6;
-
-public class ceshi {
-    public static void main(String[] args) {
-        String a = "123 456";
-//        System.out.println(a.charAt(1));
-        System.out.println(Double.parseDouble(String.valueOf(a.charAt(1)-'0')));
-    }
-}
